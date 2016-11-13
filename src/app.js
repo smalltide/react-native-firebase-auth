a98b2be6ab1eca516f623a4de76547d7d4a05413
ice <smalltides@gmail.com>
2016-11-14 00:02:11 +0800

    add firebase config

--- src/app.js
+++ src/app.js
@@ -3,9 +3,21 @@ import {
   Text,
   View
 } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyC6SRauUqJVPUgLO43WQMhuFNMutTHaI0w',
+      authDomain: 'react-native-auth-9196b.firebaseapp.com',
+      databaseURL: 'https://react-native-auth-9196b.firebaseio.com',
+      storageBucket: 'react-native-auth-9196b.appspot.com',
+      messagingSenderId: '559965301438'
+    });
+  }
+
   render() {
     return (
       <View>
