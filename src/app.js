d3c17741ed72b2122a9b92ac902c4bf98107ecab
ice <smalltides@gmail.com>
2016-11-13 23:24:09 +0800

    add common components

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+import { Header } from './components/common';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        <Text>An APP</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
