8b7ec1fd027fef14b43af7f6a7cfda90753ebe84
ice <smalltides@gmail.com>
2016-11-14 01:10:12 +0800

    add Input component for email and pwd field

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
+  state = { email: '', password: '' };
+
   render() {
     return (
       <Card>
         <CardSection>
+          <Input
+            placeholder="user@gmail.com"
+            label="Email"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            secureTextEntry
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
         </CardSection>
 
          <CardSection>
